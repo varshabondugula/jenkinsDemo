979668741c4f540ee3d294d36c57bb6ddf3b9d4f
varshabondugula <varshamudhiraj16@gmail.com>
2019-07-06 17:44:10 -0400

    added exception for build failure

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,20 @@
 
 public class HelloWorld {
+ String str = null;
+
+public void forError()
+{
+System.out.println(str.length());
+
+}
+
     public static void main(String a[]){
-        
-        System.out.println("test class for jenkins....");}
+
+HelloWorld helloWorld = new HelloWorld(); 
+       helloWorld.forError();
+        System.out.println("test class for jenkins....");
+
+}
         
     }
     
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # jenkinsDemo
+
+added exception 
